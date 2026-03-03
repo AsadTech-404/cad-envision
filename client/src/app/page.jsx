f56a04b3d46a676df5459ea26c85c81cad5a1004
Muhammad Asad <113167175+AsadTech-404@users.noreply.github.com>
2026-03-03 16:03:04 +0500

    Add margin-top to TrustStats component

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -161,7 +161,7 @@ export default function Home() {
 
       <Hero categories={categories} loading={loading} />
       
-      <TrustStats />
+      <TrustStats className="mt-10"/>
 
       <TechnicalLibrary categories={categories} loading={loading} />
 
